47674cc9a1a23656117fea11c0ee86ac1349da7f
Alex <mapleoneup@gmail.com>
2025-05-02 21:14:46 -0600

    Increase Ubuntu version required
    Remove unnecessary stuff no longer needed with newest version of Ubuntu

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -13,7 +13,7 @@ echo -n "Adding Mesa repo... "
 UBUNTU_VERSION=$(lsb_release -rs)
 UBUNTU_CODENAME=$(lsb_release -sc)
 
-if (( $(echo "$UBUNTU_VERSION >= 18.04" | bc -l) )); then
+if (( $(echo "$UBUNTU_VERSION >= 25.04" | bc -l) )); then
     echo -n "Ubuntu $UBUNTU_VERSION ($UBUNTU_CODENAME) detected... "
 
     GPG_KEYRING_PATH="/usr/share/keyrings/mesarc.gpg"
@@ -30,10 +30,6 @@ else
     exit 1
 fi
 
-# Set RADV_DEBUG environment variable globally
-echo -n "Configuring RADV_DEBUG environment variable... "
-echo 'RADV_DEBUG=nocompute' > /etc/environment
-
 # Install Oberon GPU governor
 # Fork by mothenjoyer69, originally by Segfault
 echo "Installing Oberon GPU governor..."
@@ -41,21 +37,6 @@ apt install libdrm-dev cmake make g++ git -y
 git clone https://gitlab.com/mothenjoyer69/oberon-governor.git && cd oberon-governor
 cmake . && make && make install
 systemctl enable oberon-governor.service
-
-# Apply kernel module options and regenerate initramfs
-echo -n "Configuring amdgpu kernel module options... "
-echo 'options amdgpu sg_display=0' > /etc/modprobe.d/options-amdgpu.conf
-echo -n "Configuring nct6683 kernel module options... "
-echo 'nct6683' > /etc/modules-load.d/99-sensors.conf
-echo 'options nct6683 force=true' > /etc/modprobe.d/options-sensors.conf
-echo "Regenerating initramfs (this may take a few minutes)..."
-update-initramfs -u
-
-# Clean up GRUB configuration and regenerate GRUB config
-echo "Updating GRUB configuration..."
-sed -i 's/nomodeset//g' /etc/default/grub
-sed -i 's/amdgpu\.sg_display=0//g' /etc/default/grub
-update-grub
 
 # Final notification and system reboot
 echo "Configuration complete. The system will reboot in 5 seconds. Press Ctrl+C to cancel."
