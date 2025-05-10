22363666038b3d996dbd06fbd1a1dc83382bfa23
Alex <mapleoneup@gmail.com>
2025-05-09 22:21:54 -0600

    Bring back GRUB update

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -7,44 +7,30 @@ if [[ $(id -u) != "0" ]]; then
     exit 1
 fi
 
-# List of Mesa 25.1.0 Fedora 43 RPM URLs
-MESA_URLS=(
-  "https://kojipkgs.fedoraproject.org//packages/mesa/25.1.0/1.fc43/x86_64/mesa-libGL-25.1.0-1.fc43.x86_64.rpm"
-  "https://kojipkgs.fedoraproject.org//packages/mesa/25.1.0/1.fc43/x86_64/mesa-dri-drivers-25.1.0-1.fc43.x86_64.rpm"
-  "https://kojipkgs.fedoraproject.org//packages/mesa/25.1.0/1.fc43/x86_64/mesa-filesystem-25.1.0-1.fc43.x86_64.rpm"
-  "https://kojipkgs.fedoraproject.org//packages/mesa/25.1.0/1.fc43/x86_64/mesa-libEGL-25.1.0-1.fc43.x86_64.rpm"
-  "https://kojipkgs.fedoraproject.org//packages/mesa/25.1.0/1.fc43/x86_64/mesa-va-drivers-25.1.0-1.fc43.x86_64.rpm"
-  "https://kojipkgs.fedoraproject.org//packages/mesa/25.1.0/1.fc43/x86_64/mesa-vulkan-drivers-25.1.0-1.fc43.x86_64.rpm"
-  "https://kojipkgs.fedoraproject.org//packages/mesa/25.1.0/1.fc43/x86_64/mesa-libgbm-25.1.0-1.fc43.x86_64.rpm"
-  "https://kojipkgs.fedoraproject.org//packages/mesa/25.1.0/1.fc43/x86_64/mesa-libgbm-devel-25.1.0-1.fc43.x86_64.rpm"
-)
+# Check if it's Fedora 42
+if [ "$fedora_version" != "42" ]; then
+  echo "This script is designed to run only on Fedora 42. Exiting."
+  exit 1
+fi
 
-# Directory for temporary downloads
-DOWNLOAD_DIR="/tmp/mesa-rpms"
-mkdir -p "$DOWNLOAD_DIR"
+# Configure COPR repository
+dnf copr enable danayer/mesa-git -y
+dnf upgrade -y
 
-echo "Downloading Mesa 25.1.0 RPMs with curl..."
-for url in "${MESA_URLS[@]}"; do
-  echo "Downloading $(basename "$url")..."
-  curl -L -o "$DOWNLOAD_DIR/$(basename "$url")" "$url"
-done
-
-echo "Installing Mesa 25.1.0 RPMs..."
-sudo rpm -Uvh "$DOWNLOAD_DIR"/mesa*.rpm
-
-echo "Cleaning up..."
-rm -rf "$DOWNLOAD_DIR"
-
-echo "Verification:"
+echo "Verifying Mesa version:"
 rpm -q mesa-libGL
 
-# Install Oberon GPU governor
-# Thanks to mothenjoyer69 and Segfault
+# Install Oberon GPU governor. Credit to Segfault and mothenjoyer69
 echo "Installing Oberon GPU governor..."
 dnf install libdrm-devel cmake make g++ git -y
 git clone https://github.com/buoyantbeaver/oberon-governor.git && cd oberon-governor
 cmake . && make && make install
 systemctl enable oberon-governor.service
+
+# Clean up GRUB configuration and regenerate GRUB config
+echo "Updating GRUB configuration..."
+sed -i 's/nomodeset//g' /etc/default/grub
+grub2-mkconfig -o /etc/grub2.cfg
 
 # Final notification and system reboot
 echo "Configuration complete. The system will reboot in 5 seconds. Press Ctrl+C to cancel."
